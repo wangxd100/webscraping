80eac3124e65ecf43fbe8660d0a08aa3978ee937
WANGXIAO <wangxd100@gmail.com>
2018-11-12 23:27:25 -0500

    test beautiful soup4 Add Requests Module Crawl div Attrs

--- requestUrl.py
+++ requestUrl.py
@@ -7,12 +7,18 @@ gitHub = requests.get('https://github.com/wangxd100')
 # print(gitHub)
 
 content = gitHub.content
-print(content)
+# print(content)
 
 soup = BeautifulSoup(content, "html.parser")
 allA = soup.findAll("a")
+title = soup.findAll("title")
+# print(title)
 
-for link in allA:
-    print(link)
-    print(link.string)
+# for link in allA:
+    # print(link)
+    # print(link.string)
 
+
+soup2 = BeautifulSoup(content, "html.parser")
+attrs = soup2.body.div.attrs
+print(attrs)
