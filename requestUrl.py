e0c2eb0df7028b2340d2ff4825debe2b811ad33c
wangxiao <wangxd100@hotmail.com>
2018-11-14 15:41:26 -0500

    add For loop syntax into beautifulSoup.py file

--- requestUrl.py
+++ requestUrl.py
@@ -4,7 +4,7 @@ import requests
 
 gitHub = requests.get('https://github.com/wangxd100')
 # return http status code 200
-# print(gitHub)
+print("status code:",gitHub.status_code)
 
 content = gitHub.content
 # print(content)
@@ -12,11 +12,19 @@ content = gitHub.content
 soup = BeautifulSoup(content, "html.parser")
 allA = soup.findAll("a")
 title = soup.findAll("title")
-# print(title)
 
-# for link in allA:
-    # print(link)
-    # print(link.string)
+print('------ print title --------')
+print(title)
+print(soup.title.string)
+print(soup.title.text)
+
+
+
+print('------ print links --------')
+for link in allA:
+    if len(link)>0:
+        #print(link)
+        print(link.string)
 
 
 soup2 = BeautifulSoup(content, "html.parser")
