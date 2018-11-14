fe8f5bfb1aa33fe0e0eef454ebc358c1da3e61ec
WANGXIAO <wangxd100@gmail.com>
2018-11-13 21:21:03 -0500

    test beautiful soup4 Add Requests Module Crawl div Attrs
    selenium module test
    find element by Tag
    find element by Class
    find element by Xpath
    find element by Css

---
+++ selenium-byCSS.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import time
+
+
+driver = webdriver.Chrome("C:\sheldon\webscraping\chromedriver.exe")
+# driver.fullscreen_window()
+
+driver.get("https://www.baidu.com")
+driver.find_element_by_css_selector("#kw").send_keys("test\n")
---
+++ selenium-byXpath.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+
+
+driver = webdriver.Chrome("C:\sheldon\webscraping\chromedriver.exe")
+# driver.fullscreen_window()
+
+driver.get("https://www.google.com")
+time.sleep(2)
+driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[4]/div[1]/div[1]/a[2]").click()
+
+
